9e826551daa342fea70ad3f3faf9d9038fc4572e
Olga <sinickaya1986@gmail.com>
2023-06-02 17:15:27 +0300

    второй вариант решения

--- yrok06/task41/Program.cs
+++ yrok06/task41/Program.cs
@@ -1,23 +1,38 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int number(string text)
+System.Console.WriteLine("Введите числа ");
+int [] arr = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
 {
-    System.Console.WriteLine(text);
-    return Convert.ToInt32(Console.ReadLine());
-}
-int NewMass = number("Введите количество чисел: ");
-int PositiveNums()
-{
-    int Positive = 0;
-    for (int i = 0; i < NewMass; i++)
+    if (arr[i] > 0)
     {
-        Console.WriteLine($"Введите число {i + 1}: ");
-        int num = Convert.ToInt32(Console.ReadLine());
-
-        if (num > 0)
-            Positive++;
+        count++;
     }
-    return Positive;
+
 }
-Console.WriteLine($"Положительных чисел: {PositiveNums()}");
+System.Console.WriteLine($"Количество положительных  чисел = {count}");
+
+
+
+// int number(string text)
+// {
+//     System.Console.WriteLine(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int NewMass = number("Введите количество чисел: ");
+// int PositiveNums()
+// {
+//     int Positive = 0;
+//     for (int i = 0; i < NewMass; i++)
+//     {
+//         Console.WriteLine($"Введите число {i + 1}: ");
+//         int num = Convert.ToInt32(Console.ReadLine());
+
+//         if (num > 0)
+//             Positive++;
+//     }
+//     return Positive;
+// }
+// Console.WriteLine($"Положительных чисел: {PositiveNums()}");
