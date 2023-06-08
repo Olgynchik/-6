c5407f2cecad0936710ec60f3e2e2e0fa8f4909e
Olga <sinickaya1986@gmail.com>
2023-06-08 18:31:37 +0300

    task64

---
+++ yrok09/task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 63:  // Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от 1 до N.  
+// N = 5 -> "1, 2, 3, 4, 5" // N = 6 -> "1, 2, 3, 4, 5, 6"  
+int ReadInt(string text) 
+{    
+ System.Console.WriteLine(text);     
+ return Convert.ToInt32(Console.ReadLine()); 
+}  
+void Numbers(int N) 
+{    
+ if(N < 1) return; 
+    Numbers(N-1);      
+System.Console.Write(N + " "); 
+} 
+ Numbers(ReadInt("Введите значение N"));
